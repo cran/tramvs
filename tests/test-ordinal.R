7a6c4d7be60981e161c80f163739cc3210c87d8e
Lucas Kook <lucasheinrich.kook@uzh.ch>
2022-07-16 08:10:02 +0000

    version 0.0-1

---
+++ tests/test-ordinal.R
@@ -0,0 +1,23 @@
+# Demo ordinal
+
+# Windows diffs...
+old <- options(digits = 3)
+
+set.seed(24101968)
+library(tramvs)
+
+data("wine", package = "ordinal")
+wine$noise <- rnorm(nrow(wine))
+
+# Estimate support size via HBIC
+res <- tramvs(rating ~ temp + contact + noise, data = wine, modFUN = Polr)
+plot(res, type = "b")
+plot(res, which = "path")
+
+coef(res)
+coef(res, with_baseline = TRUE)
+
+# Active set
+support(res)
+
+options(old)
